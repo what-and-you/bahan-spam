2cc8ff6b704e4443c32b284d8cf6c2bffa4b6d36
REN9999 <sejatipria990@gmail.com>
2024-08-03 15:11:49 +0700

    Update belajar_bash123.sh

--- belajar_bash123.sh
+++ belajar_bash123.sh
@@ -1,19 +1,34 @@
+function show_menu {
 echo "
           |=================================================|
           |1.MENU SPAM                                      |
           |2.MENU SPAM                                      |
           |=================================================|
 "
-          read -p "pilih menu: " p
+}
+function show_menu1 {
+sleep 1.5
+clear
+apt update && apt upgrade
+apt install git -y
+apt install python3-pip
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip3 insttill -r install.txt
+python3 brutal.py
+}
 
-          if [ $p == 1 ]
-          then
-          pkg update
-          pkg upgrade
-          fi
-          if
-          if [ $p == 2 ]
-          then
-          pkg upgrade
-          pkg install git
-          fi
+function show_menu2 {
+sleep 1.5
+clear
+apt update && apt upgrade
+pkg install bash curl wget jq
+pkg install python python-pip
+pkg install make clang
+pkg install git
+git clone https://github.com/Sxp-ID/Brutal-W4.git
+cd Brutal-W4
+git pull
+make install
+./main
+}
